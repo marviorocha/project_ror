7e2b70ef92cfcf14356b6132f7416569098c9d6f
Marvio Rocha <marviorocha@marviorocha.com>
2023-04-05 22:41:12 -0300

    test 14

--- Procfile
+++
@@ -1,4 +0,0 @@
-web: bin/rails server -p 3000
-css: yarn build:css --watch
-js: yarn build --watch
-release: bin/rails db:create db:migrate
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :authors
-  get 'home/index'
+  root 'posts#index'
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
